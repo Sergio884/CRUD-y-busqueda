0878c0bfbf58cbf16ec112170f7e966aa79ce27f
Edilberto Sergio Valle Ortiz <edilberto.sergio.valle.ortiz@gmail>
2021-04-06 12:46:18 -0500

    Cambios en loadHTMLTable

--- cliente/index.js
+++ cliente/index.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded',function (){
     fetch('http://localhost:5000/getAll')
     .then(response=>response.json())
-    .then(data=> console.log(data));
-    cargarTablaHTML([]);
+    .then(data=> loadHTMLTable(data['data']));
 });
 
-function cargarTablaHTML(data){
+function loadHTMLTable(data){
     const table = document.querySelector('table tbody');
+
+    console.log(data);
 
     if(data.length === 0){
         table.innerHTML= "<tr><td class='vacio' colspan='5'>Tabla Vacia</td></tr>";
