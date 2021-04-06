3b6364f341ac099fec25dfe34c86f45e7a4ccb78
Edilberto Sergio Valle Ortiz <edilberto.sergio.valle.ortiz@gmail>
2021-04-05 23:00:47 -0500

    Correciones en el index.js

--- cliente/index.js
+++ cliente/index.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded',function(){
     cargarTablaHTML([]);
-})
+});
 
 function cargarTablaHTML(data){
-    const tabla = document.querySelector('table tbody');
+    const table = document.querySelector('table tbody');
 
     if(data.length === 0){
-        tabla.innerHTML= "<tr><td class='vacio' colspan='5'>Tabla Vacia</td></tr>"
+        table.innerHTML= "<tr><td class='vacio' colspan='5'>Tabla Vacia</td></tr>";
     }
 }
+
